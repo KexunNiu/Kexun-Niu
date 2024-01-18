6c09bd5bb1e557b9da1980947b2826fb22e643b9
Kexun Niu <kexun@ualberta.ca>
2024-01-18 15:47:34 -0500

    Added navbar

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 
 //Components
+import NavComponent from "./components/NavComponent";
 import HomeComponent from "./components/HomePage.component";
 
 function App() {
   return (
     <div className="App">
+      <NavComponent />
       <Routes>
         <Route path="/" element={<HomeComponent />} />
       </Routes>
