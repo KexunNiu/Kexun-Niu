83921f6cf70587a7ac5388f2342c4426b538f809
Kexun Niu <kexun@ualberta.ca>
2024-01-18 15:21:42 -0500

    Add homepage

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
+import React from "react";
+import { Routes, Route } from "react-router-dom";
 
+//Components
+import HomeComponent from "./components/HomePage.component";
 
 function App() {
   return (
     <div className="App">
+      <Routes>
+        <Route path="/" element={<HomeComponent />} />
+      </Routes>
     </div>
   );
 }
---
+++ src/components/HomePage.component.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const HomeComponent = () => {
+  return (
+    <div>
+    </div>
+  );
+};
+
+export default HomeComponent;
